7daca3b57d2713232f4a1d95bb36fd2d2cb6f170
manideep lanka <mlanka@rapidbizapps.com>
2016-05-12 18:16:12 +0530

    zip/concat operators

--- app/src/main/java/com/rapidbizapps/rxsample/MainActivity.java
+++ app/src/main/java/com/rapidbizapps/rxsample/MainActivity.java
@@ -6,7 +6,10 @@ import android.util.Log;
 
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> myObservable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                String data = null;
+                try {
+                    data = fetchData();
+                    subscriber.onNext(data);
+                    subscriber.onCompleted();
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+
+
+        myObservable.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("Done");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.println(s);
+                    }
+                });
     }
+
+    private static String fetchData() throws Exception {
+        return "I solemnly swear that I am upto no good";
+    }
+    
 }
--- app/src/main/java/com/rapidbizapps/rxsample/RxSample.java
+++ app/src/main/java/com/rapidbizapps/rxsample/RxSample.java
@@ -4,9 +4,13 @@ import android.util.Log;
 
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.functions.Func2;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by mlanka on 12-05-2016.
@@ -14,34 +18,55 @@ import rx.functions.Func1;
 public class RxSample {
 
     public static void main(String[] args) {
-        //emits data only when it has an observer - Cold Observable
-        //TODO how to make this 'hot'?
-        Observable.from(new String[]{
-                "Arthur", "Molly", "Bill", "Charlie", "Percy", "Fred", "George", "Ron", "Ginny"
-                })  //emits each item from the array
-                .skip(2) //skips the first two items
-                .map(new Func1<String, String>() {
-                    @Override
-                    public String call(String s) {
-                        return s + " Weasley";
-                    }
-                }) //maps each item to a string attached with Weasley to the original item
-                .subscribe(new Observer<String>() {
-                    @Override
-                    public void onCompleted() {
+        Observable<Integer> myObservable1 = Observable.from(new Integer[]{1, 2, 3, 4, 5, 6});
 
-                    }
+        Observable<Integer> myObservable2 = Observable.from(new Integer[]{0, 32, 33, 34, 35, 36});
 
-                    @Override
-                    public void onError(Throwable e) {
+        //do something after two Observables
+        Observable<Integer> zipped = Observable.zip(myObservable1, myObservable2, new Func2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                return integer / integer2;
+            }
+        });
 
-                    }
 
-                    @Override
-                    public void onNext(String s) {
-                        System.out.println(s);
-                    }
-                });
+        zipped.subscribe(new Observer<Integer>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Done");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+
+        //emit the results one after the other
+        Observable<Integer> concatenated = Observable.concat(myObservable1,myObservable2);
+
+        concatenated.subscribe(new Observer<Integer>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println(integer + "");
+            }
+        });
 
     }
 }
