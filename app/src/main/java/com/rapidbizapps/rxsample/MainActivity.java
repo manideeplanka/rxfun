4a878add8ab47154193f304e06ca28b25bd587df
manideep lanka <mlanka@rapidbizapps.com>
2016-05-13 19:06:30 +0530

    flatMap() and reduce() operators

--- app/src/main/java/com/rapidbizapps/rxsample/MainActivity.java
+++ app/src/main/java/com/rapidbizapps/rxsample/MainActivity.java
@@ -2,21 +2,27 @@ package com.rapidbizapps.rxsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.jakewharton.rxbinding.view.RxView;
+import java.util.ArrayList;
+import java.util.List;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.functions.Func2;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     Button button;
+    List<String> names = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +31,32 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button);
 
-        //emits a string,terminates the execution
-        Observable.OnSubscribe<String> observableAction = new Observable.OnSubscribe<String>() {
+        names.add("Sorcerer's Stone");
+        names.add("Chamber of Secrets");
+        names.add("Prisoner of Askaban");
+        names.add("Goblet tof Fire");
+        names.add("Order of the Pheonix");
+        names.add("Half-blood Prince");
+        names.add("Deathly Hallows");
+
+
+        Func1<List<String>, Observable<String>> getUrls = new Func1<List<String>, Observable<String>>() {
             @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("Hello World!");
-                subscriber.onCompleted();
+            public Observable<String> call(List<String> strings) {
+                return Observable.from(strings);
             }
         };
 
-        Observable<String> observable = Observable.create(observableAction);
+        //crashes when there are no elements in stream
+        Action1<String> toastOnNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        };
 
-        Subscriber<String> buttonSubscriber = new Subscriber<String>() {
+        //does not crash. error handling
+       /* Subscriber<String> subscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
 
@@ -44,35 +64,28 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(Throwable e) {
-
+                Log.d(LOG_TAG, "onError: " + e.getMessage());
             }
 
             @Override
             public void onNext(String s) {
-                button.setText(s);
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        };*/
+
+        Func2<String, String, String> mergeRoutine = new Func2<String, String, String>() {
+            @Override
+            public String call(String s, String s2) {
+                return s + "\n" + s2;
             }
         };
 
 
-        Subscriber<String> toastSubscriber = new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                Toast.makeText(MainActivity.this, "Rx is so cool!", Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        observable.subscribe(buttonSubscriber);
-        observable.subscribe(toastSubscriber);
+        Observable.just(names)
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(getUrls)
+                .reduce(mergeRoutine)
+                .subscribe(toastOnNextAction);
     }
 }
 
