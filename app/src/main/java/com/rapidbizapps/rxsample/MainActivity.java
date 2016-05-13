9082f92c20326c128f68f3f4ebb462096ab7d4b5
manideep lanka <mlanka@rapidbizapps.com>
2016-05-13 12:27:35 +0530

    Observable.OnSubscribe implementation

--- app/src/main/java/com/rapidbizapps/rxsample/MainActivity.java
+++ app/src/main/java/com/rapidbizapps/rxsample/MainActivity.java
@@ -2,13 +2,13 @@ package com.rapidbizapps.rxsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.jakewharton.rxbinding.view.RxView;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
@@ -25,16 +25,54 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button);
 
-        Observable<Void> clicks = RxView.clicks(button);
+        //emits a string,terminates the execution
+        Observable.OnSubscribe<String> observableAction = new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello World!");
+                subscriber.onCompleted();
+            }
+        };
 
-        clicks.skip(3)
-                .subscribe(new Action1<Void>() {
-                    @Override
-                    public void call(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
-                    }
-                });
+        Observable<String> observable = Observable.create(observableAction);
+
+        Subscriber<String> buttonSubscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                button.setText(s);
+            }
+        };
 
 
+        Subscriber<String> toastSubscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Toast.makeText(MainActivity.this, "Rx is so cool!", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        observable.subscribe(buttonSubscriber);
+        observable.subscribe(toastSubscriber);
     }
 }
+
